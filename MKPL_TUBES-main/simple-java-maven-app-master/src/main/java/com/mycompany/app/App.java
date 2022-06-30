66e6ca6e2bbea2c2c034867ff8eb2a3eb91abd5d
reyhanlatuconsina <55250316+reyhanlaconsina@users.noreply.github.com>
2022-06-30 16:41:53 +0700

    Update App.java

--- MKPL_TUBES-main/simple-java-maven-app-master/src/main/java/com/mycompany/app/App.java
+++ MKPL_TUBES-main/simple-java-maven-app-master/src/main/java/com/mycompany/app/App.java
@@ -12,6 +12,8 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+        System.out.println(new App().getMessage());
+        System.out.println(new App().getMessage());
     }
 
     private final String getMessage() {
